855ec026d4d6350b139fd389b9696752d755ae2e
Bruno Henrique Silva Viola <99220989+BrunoViola@users.noreply.github.com>
2025-10-21 22:08:27 -0300

    Refactor product listing to use blade view
    Updated ProdutoController to return a Blade view ('listagem') instead of generating HTML in the controller. Added new 'listagem.php' view and included a CSS file for styling. Removed the default 'welcome.blade.php' view.

--- AppEstoque/app/Http/Controllers/ProdutoController.php
+++ AppEstoque/app/Http/Controllers/ProdutoController.php
@@ -6,15 +6,8 @@
    class ProdutoController extends Controller {
       public function lista () {
          $produtos = DB::select('SELECT * FROM produtos');
-         $html = '<h1>Listagem de Produtos</h1><ul>';
-
-         foreach ($produtos as $p) {
-            $html .= '<li>' . $p->nome . '</li>';
-            $html .= ', Descrição: ' . $p->descricao . '</li>';
-         }
-
-         $html .= '</ul>';
-         return $html;
+         
+         return view('listagem')->with('produtos', $produtos);
       }
    }
 ?>
