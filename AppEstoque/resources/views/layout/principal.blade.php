dc6b9bf9404774166d81b64e2faa4472089549ca
Bruno Henrique Silva Viola <99220989+BrunoViola@users.noreply.github.com>
2025-10-22 22:35:05 -0300

    add bootstrap 3 using cdn

--- AppEstoque/resources/views/layout/principal.blade.php
+++ AppEstoque/resources/views/layout/principal.blade.php
@@ -5,7 +5,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-   <link rel="stylesheet" href="/css/app.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
    <link rel="stylesheet" href="/css/custom.css">
 
    <title>Controle de Estoque</title>
